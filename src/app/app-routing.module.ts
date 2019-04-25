09960e9065b630e1650f5c77bc2876028ab0262b
Milton Fernado Molloja <milton.molloja@gmail.com>
2019-04-25 04:06:35 -0300

    Componetes Agregados - Anda

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
-import { NgModule } from '@angular/core';
+import { NgModule, Component } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HeaderComponent } from './component/header/header.component';
+import { FooterComponent } from './component/footer/footer.component';
+import { ContentsComponent } from './component/contents/contents.component';
+import { ContactComponent } from './component/contact/contact.component';
+import { RegistryComponent } from './component/registry/registry.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'contents', component: ContentsComponent},
+  {path: 'header', component: HeaderComponent},
+  {path: 'footer', component: FooterComponent},
+  {path: 'contact', component: ContactComponent},
+  {path: 'registry', component: RegistryComponent},
+  {path: '**', component: ContentsComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,8 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './component/header/header.component';
 import { FooterComponent } from './component/footer/footer.component';
 import { ContentsComponent } from './component/contents/contents.component';
+import { ContactComponent } from './component/contact/contact.component';
+import { RegistryComponent } from './component/registry/registry.component';
 
 
 @NgModule({
@@ -14,6 +16,8 @@ import { ContentsComponent } from './component/contents/contents.component';
     HeaderComponent,
     FooterComponent,
     ContentsComponent,
+    ContactComponent,
+    RegistryComponent,
 
   ],
   imports: [
